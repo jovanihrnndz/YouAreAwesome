c7843a7ac051751b9ea0a04c7a760f4a630d9677
jovanihrnndz <jovanihrnndz@outlook.com>
2025-01-08 22:45:50 -0800

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by jovanihrnndz on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
