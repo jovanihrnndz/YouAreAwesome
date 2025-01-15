d3f6b2ad19e03aec031565f5ab91b656c2a96f6b
Jovani Hernandez <jovanihrnndz@outlook.com>
2025-01-14 23:41:50 -0800

    Updated after first lesson

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            Image(systemName: "cloud.sun.fill")
+                .resizable()
+                .scaledToFit()
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+             Text("You are Awseome!" )
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
         }
         .padding()
+        
     }
 }
 
